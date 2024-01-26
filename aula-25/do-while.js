a93069f44ce266beb00524c67f2a6212353f863b
Allan Anes <allan.anes.louzada@gmail.com>
2024-01-26 14:52:16 -0300

    Aprendendo bastante com a one bit code

---
+++ aula-25/do-while.js
@@ -0,0 +1,8 @@
+let velocidade = 0
+
+do {
+    alert("A velocidade dp veículo é " + velocidade + " km/h")
+    velocidade -= 20
+} while (false)
+
+alert("velocidade final: " + velocidade + " km/h")
